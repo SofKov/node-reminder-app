81ae264aa12052604d5a2b22197d50ff58c0a14b
Sofia K <sofiakovalevskaia@gmail.com>
2021-03-23 14:05:26 +0000

    updated files, added utils and gitignore

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log(process.argv)
+const yargs = require("yargs");
+const {addNote} = require("../utils/notes");
+
+console.log(process.argv); // argv is the argument vector - it is an array which stores all the arguments which we type to the console 
+console.log(yargs.argv);
+
+const command = process.argv[2];
+
+if (command == "add"){
+    console.log("Adding new note:");
+    addNote(yargs.argv.note);
+} else if (command == "remove"){
+    console.log("Removing note:");
+} else if (command == "list"){
+    console.log("All notes added are displayed below:");
+} else {
+    console.log("command not recognised");
+};
